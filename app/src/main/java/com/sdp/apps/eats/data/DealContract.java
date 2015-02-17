254c8c92daec1c6cd95f345c95ad9078cefbb1d9
Simon du Preez <smdupnz@yahoo.co.nz>
2015-02-17 14:32:27 +1300

    Created two new files to begin with the database

---
+++ app/src/main/java/com/sdp/apps/eats/data/DealContract.java
@@ -0,0 +1,37 @@
+package com.sdp.apps.eats.data;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by Simon on 17/02/2015.
+ */
+public class DealContract {
+
+    // To prevent someone from accidentally instantiating the contract class,
+    // give it an empty constructor.
+    public DealContract(){}
+
+    /*
+        Inner class that defines the table contents of the location table
+     */
+    public static final class LocationEntry implements BaseColumns {
+        public static final String TABLE_NAME           =   "location";
+        public static final String COLUMN_CITY_NAME     =   "city_name";
+        public static final String COLUMN_COORD_LAT     =   "coord_lat";
+        public static final String COLUMN_COORD_LONG    =   "coord_long";
+    }
+
+    /* Inner class that defines the table contents of the deal table */
+    public static final class DealEntry implements BaseColumns {
+
+        public static final String TABLE_NAME               =   "deal";
+        public static final String COLUMN_LOC_KEY           =   "location_id";
+        public static final String COLUMN_BUSINESS_NAME     =   "business_name";
+        public static final String COLUMN_SHORT_DESC        =   "short_description";
+        public static final String COLUMN_LONG_DESC         =   "long_description";
+        public static final String COLUMN_PRICE             =   "price";
+        public static final String COLUMN_PHOTO_URI         =   "photo_uri";
+        public static final String COLUMN_VOUCHER_CODE      =   "voucher_code";
+    }
+
+}
---
+++ app/src/main/java/com/sdp/apps/eats/data/DealDbHelper.java
@@ -0,0 +1,47 @@
+package com.sdp.apps.eats.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Simon on 17/02/2015.
+ */
+public class DealDbHelper extends SQLiteOpenHelper{
+
+    private static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "deals.db";
+
+    public DealDbHelper(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db){
+        final String SQL_CREATE_DEAL_TABLE = "CREATE TABLE " +
+                DealContract.DealEntry.TABLE_NAME + " (" +
+                DealContract.DealEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"+
+                DealContract.DealEntry.COLUMN_LOC_KEY + "INTEGER NOT NULL, "+
+                DealContract.DealEntry.COLUMN_BUSINESS_NAME + "TEXT NOT NULL,"+
+                DealContract.DealEntry.COLUMN_SHORT_DESC + "TEXT NOT NULL,"+
+                DealContract.DealEntry.COLUMN_LONG_DESC + "TEXT,"+
+                DealContract.DealEntry.COLUMN_PRICE + "REAL NOT NULL,"+
+                DealContract.DealEntry.COLUMN_PHOTO_URI + "TEXT,"+
+                DealContract.DealEntry.COLUMN_VOUCHER_CODE + "TEXT" +
+
+                // Set up the location column as a foreign key to the location table
+                " FOREIGN KEY (" + DealContract.DealEntry.COLUMN_LOC_KEY + ") REFERENCES" +
+                DealContract.LocationEntry.TABLE_NAME + " (" + DealContract.LocationEntry._ID +"),";
+
+        db.execSQL(SQL_CREATE_DEAL_TABLE);
+
+    }
+
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // This database is only a cache for online data, so its upgrade policy is
+        // to simply to discard the data and start over
+        db.execSQL("DROP TABLE IF EXISTS " + DealContract.LocationEntry.TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + DealContract.DealEntry.TABLE_NAME);
+        onCreate(db);
+    }
+}
